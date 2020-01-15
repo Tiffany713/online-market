1621b576330128a73b0fd2133810729f9f3490f3
Rachel Schieferstein and Tiffany Nguyen <rachel@>
2020-01-15 15:13:53 -0800

    added complete street address division form fields

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 $(document).ready(function() {
   $("#formName").submit(function(event) {
+   
     var firstNameInput = $("input#firstName").val();
-
     $(".firstName").text(firstNameInput);
-  
+
+
     var lastNameInput = $("input#lastName").val()
     ;
-
     $(".lastName").text(lastNameInput);
 
-    var addressInput = $("input#address").val();
 
-    $(".address").text(addressInput);
+    var streetInput = $("input#street").val();
+    $(".street").text(streetInput);
+
+    var cityInput = $("input#city").val();
+    $(".city").text(cityInput);
+
+    var stateInput = $("input#state").val();
+    $(".state").text(stateInput);
+
+    var zipInput = $("input#zip").val();
+    $(".zip").text(zipInput);
 
     $("#reciept").show();
 
